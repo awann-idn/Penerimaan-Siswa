94cd1c937871b0ef08dc86cc3e5abf334c4faebf
Nuredy Rahma Gunawan <naawaa1903@gmail.com>
2026-05-05 06:45:48 +0700

    debug: simplify start command and fix logs

--- start.sh
+++ start.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Pastikan folder storage dan cache bisa ditulisi
+# Pastikan folder storage bisa ditulisi
 chmod -R 775 storage bootstrap/cache
-php artisan storage:link
 
 # Jalankan migrasi
 php artisan migrate --force
 
-# Jalankan optimasi (opsional tapi bagus untuk production)
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Jalankan server dengan router index.php (Cara paling stabil)
-php -S 0.0.0.0:$PORT -t public public/index.php
+# Jalankan server lewat Artisan Serve (lebih akurat untuk routing Laravel)
+php artisan serve --host=0.0.0.0 --port=$PORT
