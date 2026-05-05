3dbd8a647793c5556c7f3d31c5e4e26685456656
Nuredy Rahma Gunawan <naawaa1903@gmail.com>
2026-05-05 08:45:02 +0700

    fix: force storage link to work with volume

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Hapus link storage lama jika ada (agar tidak bentrok)
+rm -rf public/storage
+
 # Pastikan folder storage bisa ditulisi
 chmod -R 775 storage bootstrap/cache
+
+# Buat ulang link storage secara paksa
+php artisan storage:link
 
 # Jalankan migrasi
 php artisan migrate --force
 
-# PAKSA dengerin di port 8000 sesuai settingan Dashboard Railway Anda
+# Jalankan server
 exec php artisan serve --host=0.0.0.0 --port=8000
